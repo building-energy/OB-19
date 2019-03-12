85898351d1d8adaf82ce56e5392383b0de3a2f97
Steven Firth <s.k.firth@lboro.ac.uk>
2019-03-12 16:18:38 +0000

    updates

---
+++ analysis/config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#REFIT LOUGHBOROUGH
+
+#FULL FILEPATH OF THE 'RefitXMLSchema.xsd' FILE
+refit_schema=r'C:\Users\cvskf\OneDrive - Loughborough University\_Data\REFIT_Smart_Home_dataset_PUBLIC_v1\Data\RefitXMLSchema.xsd'
+
+#FULL FILEPATH OF THE 'REFIT_BUILDING_SURVEY.xml' FILE
+refit_building_survey=r'C:\Users\cvskf\OneDrive - Loughborough University\_Data\REFIT_Smart_Home_dataset_PUBLIC_v1\Data\REFIT_BUILDING_SURVEY.xml'
+
+#FULL FILEPATH OF THE 'REFIT_TIME_SERIES_VALUES.csv' FILE
+refit_time_series_values=r'C:\Users\cvskf\OneDrive - Loughborough University\_Data\REFIT_Smart_Home_dataset_PUBLIC_v1\Data\REFIT_TIME_SERIES_VALUES.csv'
+
+
+# REFIT STRATHCLYDE
+
+#FULL FILEPATH OF THE FOLDER CONTAINING THE 20 CSV FILES
+refit_electrical_measurements_folder=r'C:\Users\cvskf\OneDrive - Loughborough University\_Data\REFIT_Electrical_load_measurements_(cleaned)\Data'
+
+
